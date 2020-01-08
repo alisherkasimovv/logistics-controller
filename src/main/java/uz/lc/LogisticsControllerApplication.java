8735035fac7871794f383e9266b3695bea4b05e0
Alisher Kasimov <alisherkasimov1994@gmail.com>
2020-01-08 14:33:55 +0500

    New app created;

---
+++ src/main/java/uz/lc/LogisticsControllerApplication.java
@@ -0,0 +1,13 @@
+package uz.lc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsControllerApplication.class, args);
+	}
+
+}
