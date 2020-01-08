d9aca7146329b550de05f008f9e6ee56d369b399
Bunyodbek Ahmadjonov <bbekk2000@gmail.com>
2020-01-08 12:34:38 +0500

    User and Company classes are added

--- src/main/java/uz/lc/db/entities/Company.java
+++ src/main/java/uz/lc/db/entities/Company.java
@@ -1,6 +1,30 @@
 package uz.lc.db.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import uz.lc.db.entities.base.UpdateBaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "db_companies")
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Company extends UpdateBaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "longitude")
+    private String longitude;
+
+    @Column(name = "latitude")
+    private String latitude;
 }
