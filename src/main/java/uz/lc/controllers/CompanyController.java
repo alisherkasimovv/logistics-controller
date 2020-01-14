410d12c4f3ee8842794376944f53e703e4353844
Bunyodbek Ahmadjonov <bbekk2000@gmail.com>
2020-01-14 09:16:43 +0500

    User and Company controllers are compleated

--- src/main/java/uz/lc/controllers/CompanyController.java
+++ src/main/java/uz/lc/controllers/CompanyController.java
@@ -5,42 +5,43 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-import uz.lc.db.dao.interfaces.UserDAO;
-import uz.lc.db.entities.User;
+import uz.lc.db.dao.interfaces.CompanyDAO;
+import uz.lc.db.entities.Company;
 
 import java.util.List;
+
 @CrossOrigin("http://localhost:3000")
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/companies")
 @Service
-public class UserController {
-    private UserDAO dao;
+public class CompanyController {
+    private CompanyDAO dao;
 
     @Autowired
-    public UserController(UserDAO dao) {
+    public CompanyController(CompanyDAO dao) {
         this.dao = dao;
     }
 
     @GetMapping(value = "/get")
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<Company>> getAllCompany() {
         return new ResponseEntity<>(dao.getAll(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/get/{id}")
-    public ResponseEntity<User> getUser(@PathVariable int id) {
+    public ResponseEntity<Company> getCompany(@PathVariable int id) {
         return new ResponseEntity<>(dao.getById(id), HttpStatus.OK);
     }
 
     @GetMapping(value = "/delete/{id}")
-    public ResponseEntity<User> deleteUser(@PathVariable int id) {
+    public HttpStatus deleteCompany(@PathVariable int id) {
         dao.deleteById(id);
-        return new ResponseEntity<>(dao.getById(id), HttpStatus.OK);
+        return HttpStatus.OK;
     }
 
     @PostMapping(value = "/save")
-    public ResponseEntity<List<User>> saveUser(@RequestBody User user) {
-        dao.saveUser(user);
-        return new ResponseEntity<>(dao.getAll(), HttpStatus.OK);
+    public HttpStatus saveUser(@RequestBody Company company) {
+        dao.saveCompany(company);
+        return HttpStatus.OK;
     }
 
 }
